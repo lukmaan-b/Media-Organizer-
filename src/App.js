51de74c9a7650c4e34504636b29ac6b5df19c379
lukmaan-b <lukmaan.butt@gmail.com>
2020-12-10 10:47:04 +0000

    Added: Folders for components & hooks. Added: AddBar component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <h1>Media Organizer</h1>;
-}
+import React from 'react';
+import AddBar from './components/AddBar/AddBar';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Media Organizer</h1>
+      <AddBar />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/AddBar/AddBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddBar = () => {
+  return (
+    <form>
+      <input type="text" />
+      <button>Games</button>
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default AddBar;
